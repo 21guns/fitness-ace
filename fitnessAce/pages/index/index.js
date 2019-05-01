dc5fc73361ed9c5738127a77322019df2afc3e64
jliu <liujun_0810@sohu.com>
2019-05-01 11:27:24 +0800

    issue:changes paln layout

--- fitnessAce/pages/index/index.js
+++ fitnessAce/pages/index/index.js
@@ -7,6 +7,10 @@ Page({
     topics:["胸","背","腿","肩"],
     weightUnits:["kg","lbs"],
     showId:1,
+    inputValue: '',//点击结果项之后替换到文本框的值
+    adapterSource: ["平板杠铃推胸", "下斜杠铃推胸", "上斜杠铃推胸", "weixin", "WeiXin", "wechat", "android", "Android", "ios", "iOS", "java", "javascript", "微信小程序", "微信公众号", "微信开发者",
+      "微信开发者工具"],//本地匹配源
+    bindSource: [],//绑定到页面的数据，根据用户输入动态变化
     plan:{
       date:'',
       topic:0,
@@ -144,6 +148,34 @@ Page({
     let groupIndex = this._findGoupIndex(e)
     return util.findeIndexForShowId(this.data.plan.groups[groupIndex].actions, e.currentTarget.id)
   },
+  //当键盘输入时，触发input事件
+  bindinput: function (e) {
+    var prefix = e.detail.value//用户实时输入值
+    var newSource = []//匹配的结果
+    if (prefix != "") {
+      this.data.adapterSource.forEach(function (e) {
+        if (e.indexOf(prefix) != -1) {
+          newSource.push(e)
+        }
+      })
+    }
+    if (newSource.length != 0) {
+      this.setData({
+        bindSource: newSource
+      })
+    } else {
+      this.setData({
+        bindSource: []
+      })
+    }
+  },
+  itemtap: function (e) {
+    this.setData({
+      inputValue: e.target.id,
+      bindSource: []
+    })
+  },
+
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
